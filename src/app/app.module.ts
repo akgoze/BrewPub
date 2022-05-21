dd2938c5b83d67e1b0986407e8c1b99fde2eb1a2
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-21 08:44:26 +0300

    Added HttpClient module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './components/item/item.component';
@@ -14,7 +15,8 @@ import { SearchComponent } from './components/search/search.component';
     SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Beer } from '../models/Beer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
 }
