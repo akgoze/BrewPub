f59e9dbd340f301c6e8770c7e9092a7c2eec2707
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-21 07:46:52 +0300

    Added search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BeerItemComponent } from './components/beer-item/beer-item.component';
 import { ListComponent } from './components/list/list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeerItemComponent,
-    ListComponent
+    ListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
