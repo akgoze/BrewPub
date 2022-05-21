04d8cfb4b79a1003768c89ba028f60980a9a02d7
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-21 13:11:23 +0300

    Updated beer model with Volume.

--- src/app/models/Beer.ts
+++ src/app/models/Beer.ts
@@ -4,4 +4,17 @@ export interface Beer {
   tagline?: string;
   description?: string;
   image_url?: string;
+  abv?: number;
+  ph?: number;
+  attenuation_level?: number;
+  brewers_tips?: string;
+  contributed_by?: string;
+  first_brewed?: string;
+  food_pairing?: string[];
+  volume?: Volume;
+}
+
+export interface Volume {
+  value: number;
+  unit: string;
 }
