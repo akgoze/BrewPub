d3dde807ea2c61c5a69d7bbdabfddc3607583ef4
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-21 07:45:09 +0300

    Added beer-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BeerItemComponent } from './components/beer-item/beer-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerItemComponent
   ],
   imports: [
     BrowserModule
