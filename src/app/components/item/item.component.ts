467d14e12b9ae45149d2a06f2158e7b5f0491449
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-21 13:10:25 +0300

    Added beer list item.

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Beer } from 'src/app/models/Beer';
 
 @Component({
-  selector: 'app-item',
+  selector: 'list-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
+  @Input() beer: Beer = {
+    id: 0,
+    name: '',
+    tagline: '',
+    description: '',
+    image_url: '',
+    abv: undefined,
+    attenuation_level: undefined,
+    brewers_tips: '',
+    contributed_by: '',
+    first_brewed: '',
+  };
 
   constructor() { }
 
