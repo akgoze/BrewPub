8b0754232411ca7c4e1b7f614366176f0c580346
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-22 12:54:22 +0300

    Added getAllBeers method.

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -10,4 +10,10 @@ export class BeerService {
   constructor(
     private _http: HttpClient
   ) { }
+
+
+  getAllBeers():Observable<Beer[]> {
+    return this._http.get<Beer[]>('https://api.punkapi.com/v2/beers?page=1&per_page=5');
+  }
+
 }
