6a48be21bb1c1a1e870546d55a7369db826d4a8e
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-22 12:54:32 +0300

    Added search method.

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Beer } from '../models/Beer';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,7 @@ export class BeerService {
     return this._http.get<Beer[]>('https://api.punkapi.com/v2/beers?page=1&per_page=5');
   }
 
+  searchBeers(searchTerm: string):Observable<Beer[]> {
+    return this._http.get<Beer[]>(`https://api.punkapi.com/v2/beers?beer_name=${searchTerm}`);
+  }
 }
