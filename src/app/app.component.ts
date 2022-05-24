173bcc57c79b0e36e74bd9eb11566191e945ddff
Fatih Akgöze <fatihakgoze@designedbysherpa.com>
2022-05-24 09:25:56 +0300

    Added app header.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BrewPub';
+  isFilterShow:boolean = false;
+
+  getFilterStatus($event:any) {
+    this.isFilterShow = $event;
+  }
 }
